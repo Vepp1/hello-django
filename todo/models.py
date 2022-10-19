2adb0331308befdd40a4051af205144b9a3ef851
Vepp1 <vinipontarolla@gmail.com>
2022-10-19 12:40:27 +0000

    Add add_item method

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,8 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
